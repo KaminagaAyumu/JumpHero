d2af4648b89d994c91336ac20035a0496579abde
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-12-12 11:39:33 +0900

    ポーズシーンの追加

---
+++ Projects/Scene/PauseScene.cpp
@@ -0,0 +1,31 @@
+﻿#include "PauseScene.h"
+
+namespace
+{
+	constexpr int kFadeInterval = 60; // フェード処理を行う時間
+
+	constexpr int kMaxFadeRate = 255; // フェード進行率の最大値
+}
+
+PauseScene::PauseScene(SceneController& controller) :
+	SceneBase(controller)
+	//m_updateFunc(&SelectScene::FadeInUpdate),
+	//m_drawFunc(&SelectScene::FadeDraw),
+	//m_fadeColor(0x000000)
+{
+	m_frameCount = kFadeInterval;
+}
+
+PauseScene::~PauseScene()
+{
+}
+
+void PauseScene::Update(Input& input)
+{
+	//(this->*m_updateFunc)(input);
+}
+
+void PauseScene::Draw()
+{
+	//(this->*m_drawFunc)();
+}
---
+++ Projects/Scene/PauseScene.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "SceneBase.h"
+
+/// <summary>
+/// シーンを一時停止するクラス
+/// </summary>
+class PauseScene : public SceneBase
+{
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="controller">シーン管理用クラス(継承元で取得しなければならない)</param>
+	PauseScene(SceneController& controller);
+	virtual ~PauseScene();
+	/// <summary>
+	/// シーンの更新処理
+	/// </summary>
+	/// <param name="input">入力情報を取得するクラス</param>
+	void Update(Input& input) override;
+	/// <summary>
+	/// シーンの描画処理
+	/// </summary>
+	void Draw() override;
+};
+
