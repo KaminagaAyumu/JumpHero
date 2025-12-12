4e3aef7fcbe564b33a071a5c7970090c78fa0f13
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-12-12 10:45:47 +0900

    文字列変換関数クラスを追加

---
+++ Projects/StringFunction.cpp
@@ -0,0 +1,15 @@
+﻿#include "StringFunction.h"
+#include "DxLib.h"
+
+std::wstring StringFunction::WStringFromString(const std::string& str)
+{
+	std::wstring result;
+
+	int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), nullptr, 0);
+
+	result.resize(len);
+
+	len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), result.data(), result.size());
+
+    return result;
+}
---
+++ Projects/StringFunction.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <string>
+
+class StringFunction
+{
+public:
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換する
+	/// </summary>
+	/// <param name="str">マルチバイト文字列</param>
+	/// <returns>ワイド文字列</returns>
+	static std::wstring WStringFromString(const std::string& str);
+};
+
