06f1b3a23d4676b1928ccbedee91e805463dcbfe
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-12-16 11:57:16 +0900

    チュートリアルで使うイベント構造体を追加

--- Projects/Game/TutorialManager.h
+++ Projects/Game/TutorialManager.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#include <sstream>
+
 class TutorialManager
 {
 public:
@@ -7,9 +9,35 @@ public:
 
 
 private:
+	// イベントが始まる条件
+	enum class TriggerType
+	{
+		EnterArea, // エリアに入った
+		GetItem, // アイテムを取得した
+		EnemySpawn, // 敵がスポーンした
+	};
 
+	// イベントで何を行うか
+	enum class ActionType
+	{
+		ShowText, // テキストを表示する
+		FreezeGame, // ゲームを一時停止する
+		UnfreezeGame, // ゲームを再開する
+		PowerUp, // プレイヤーの強化
+		WaitInput, // ボタン入力待ち
+	};
 
-
+	/// <summary>
+	/// チュートリアルで行われるイベントをまとめる構造体
+	/// </summary>
+	struct EventData
+	{
+		int id; // 識別ID
+		TriggerType triggerType; // イベントが始まる条件
+		std::string triggerParam; // イベントの場合分けで必要なデータ
+		ActionType actionType; // 行われるイベント
+		std::string actionParam; // イベントで必要なデータ
+	};
 
 };
 
