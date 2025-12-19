6a057c22bdfbcb82632fc7515b7269aee796990c
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-12-19 15:57:58 +0900

    イベント管理用構造体を追加

---
+++ Projects/Game/EventControls.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <functional>
+#include <string>
+
+/// <summary>
+/// イベントの内容をまとめた構造体
+/// </summary>
+struct EventControls
+{
+	// ヒントを表示する関数
+	std::function<void(const std::string& textId)> showHintFunc;
+
+};
