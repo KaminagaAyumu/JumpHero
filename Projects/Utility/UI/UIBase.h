8faa1d0fefe023621dbf977027871bdb7ece9894
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-12-19 16:32:42 +0900

    UIの基底クラスを作成

---
+++ Projects/Utility/UI/UIBase.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+/// <summary>
+/// UI関連の基底クラス
+/// </summary>
+class UIBase
+{
+public:
+	virtual ~UIBase() = default;
+	virtual void Update()abstract;
+	virtual void Draw()abstract;
+};
+
