5420dd70b0d2330c1538186528fc22ccba791825
KaminagaAyumu <kaminagajobsearch@gmail.com>
2025-12-19 17:00:18 +0900

    変数を定義中

--- Projects/Utility/UI/UITextWindow.h
+++ Projects/Utility/UI/UITextWindow.h
@@ -15,8 +15,10 @@ public:
 	void Draw()const override;
 
 private:
-	Position2 m_pos; // 表示座標(中心座標)
+	Position2 m_pos; // 現在の表示座標(中心座標)
+	Position2 m_targetPos; // 指定位置に移動するための座標
 	std::string m_text; // 表示するテキスト
+	Size m_size; // ウィンドウのサイズ(widthとheight)
 
 };
 
