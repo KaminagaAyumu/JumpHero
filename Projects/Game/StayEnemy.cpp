118390a4ef032b832fe560c0cd54a3caeb2c0361
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-07 22:58:45 +0900

    動かない敵クラスを追加

---
+++ Projects/Game/StayEnemy.cpp
@@ -0,0 +1,27 @@
+﻿#include "StayEnemy.h"
+
+StayEnemy::StayEnemy(const Position2& pos, Player* player, Map* map) :
+	EnemyBase(player, map)
+{
+	m_pos = pos;
+}
+
+void StayEnemy::Init()
+{
+}
+
+void StayEnemy::Update(Input&)
+{
+}
+
+void StayEnemy::Draw()
+{
+}
+
+void StayEnemy::ChangeToItem(int time)
+{
+}
+
+void StayEnemy::IsCollision(const Types::CollisionInfo& info)
+{
+}
---
+++ Projects/Game/StayEnemy.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "EnemyBase.h"
+
+/// <summary>
+/// 動かない敵クラス
+/// </summary>
+class StayEnemy : public EnemyBase
+{
+	StayEnemy(const Position2& pos, Player* player, Map* map);
+
+	virtual ~StayEnemy() = default;
+	virtual void Init() override;
+	virtual void Update(Input&) override;
+	virtual void Draw() override;
+
+	virtual void ChangeToItem(int time) override;
+
+private:
+
+	virtual void IsCollision(const Types::CollisionInfo& info) override;
+};
+
