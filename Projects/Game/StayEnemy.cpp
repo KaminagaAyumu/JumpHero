099c58cead581999d49c8ac0799a8ba82e8798d0
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-08 11:07:52 +0900

    動かない敵の更新と描画処理をできるよう実装

--- Projects/Game/StayEnemy.cpp
+++ Projects/Game/StayEnemy.cpp
@@ -1,21 +1,44 @@
 ﻿#include "StayEnemy.h"
 
+namespace
+{
+	// 描画関連
+	constexpr float kWidth = 40.0f;	// 敵の実際の幅
+	constexpr float kHeight = 40.0f;	// 敵の実際の高さ
+
+	constexpr float kCoinWidth = 30.0f;	// 敵がコイン化した際の幅
+	constexpr float kCoinHeight = 30.0f;	// 敵がコイン化した際の高さ
+
+	// 更新処理関連
+	constexpr int	kDeadTime = 300;		// 敵がやられて消えるの時間
+
+	// 演出関連
+	constexpr float	kItemWarningRate = 0.20f;	// アイテム化が終わりそうなことを示す時間の割合
+	constexpr float kWarningFrashCycle = 0.30f;	// 点滅の周期
+	constexpr int	kMaxFadeRate = 255;		// フェード率の最大値
+}
+
 StayEnemy::StayEnemy(const Position2& pos, Player* player, Map* map) :
 	EnemyBase(player, map)
 {
+	m_updateFunc = &StayEnemy::NormalUpdate;
 	m_pos = pos;
 }
 
 void StayEnemy::Init()
 {
+	m_colRect = { m_pos,kWidth,kHeight };
+	m_colCircle = { m_pos,kWidth / 2 };
 }
 
-void StayEnemy::Update(Input&)
+void StayEnemy::Update(Input& input)
 {
+	(this->*m_updateFunc)(input);
 }
 
 void StayEnemy::Draw()
 {
+	(this->*m_drawFunc)();
 }
 
 void StayEnemy::ChangeToItem(int time)
@@ -24,4 +47,26 @@ void StayEnemy::ChangeToItem(int time)
 
 void StayEnemy::IsCollision(const Types::CollisionInfo& info)
 {
+}
+
+void StayEnemy::NormalUpdate(Input&)
+{
+}
+
+void StayEnemy::ItemUpdate(Input&)
+{
+}
+
+void StayEnemy::DeadUpdate(Input&)
+{
+}
+
+void StayEnemy::NormalDraw()
+{
+
+}
+
+void StayEnemy::ItemDraw()
+{
+
 }
--- Projects/Game/StayEnemy.h
+++ Projects/Game/StayEnemy.h
@@ -18,5 +18,19 @@ class StayEnemy : public EnemyBase
 private:
 
 	virtual void IsCollision(const Types::CollisionInfo& info) override;
+
+	// 更新処理用関数群
+	using UpdateFunc_t = void(StayEnemy::*)(Input&); // 更新処理用関数ポインタの型定義
+	UpdateFunc_t m_updateFunc; // 現在の更新処理用関数ポインタ
+	void NormalUpdate(Input&); // 通常時の更新処理
+	void ItemUpdate(Input&);   // アイテム変化時の更新処理
+	void DeadUpdate(Input&);   // 倒された後の更新処理
+
+	// 描画処理用関数群
+	using DrawFunc_t = void(StayEnemy::*)(); // 描画処理用関数ポインタの型定義
+	DrawFunc_t m_drawFunc; // 現在の描画処理用関数ポインタ
+	void NormalDraw(); // 通常時の描画処理
+	void ItemDraw();   // アイテム変化時の描画処理
+
 };
 
