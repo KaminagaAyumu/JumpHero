e58c07ed677551857021f11c96146c09c7cdd784
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-08 23:36:06 +0900

    アニメーションクラスの仕様を実装

--- Projects/Utility/Animation.cpp
+++ Projects/Utility/Animation.cpp
@@ -1,9 +1,80 @@
-﻿#include "Animation.h"
+﻿#include "Geometry.h"
+#include "Animation.h"
+#include "DxLib.h"
 
-Animation::Animation()
+namespace
+{
+	constexpr int kAnimNumMin = 1; // アニメーションの最小枚数
+}
+
+Animation::Animation() : 
+	m_handle(-1),
+	m_animSize({ 0,0 }),
+	m_animNum(0),
+	m_animFrame(0),
+	m_animIndex(0),
+	m_isLoop(false),
+	m_animAngle(0.0f),
+	m_animScale(1.0f),
+	m_frameCount(0),
+	m_animCount(0)
 {
 }
 
 Animation::~Animation()
 {
 }
+
+void Animation::SetAnimation(int handle, const Size& animSize, int animNum, int animFrame, bool isLoop)
+{
+	m_handle = handle;
+	m_animSize = animSize;
+	m_animNum = animNum;
+	m_animFrame = animFrame;
+	m_isLoop = isLoop;
+	m_animIndex = 0;
+	m_frameCount = 0;
+}
+
+void Animation::Update()
+{
+	if (m_animNum <= kAnimNumMin)
+	{
+		return; // アニメーションが1枚以下なら更新しない
+	}
+	m_frameCount++;
+	if (m_frameCount >= m_animFrame)
+	{
+		m_frameCount = 0;
+		m_animCount++;
+		if (m_animCount >= m_animNum)
+		{
+			if (m_isLoop)
+			{
+				m_animCount = 0; // ループするなら最初に戻る
+			}
+			else
+			{
+				m_animCount = m_animNum - kAnimNumMin; // ループしないなら最後のフレームで止める(カウントが0から始まるため-1)
+			}
+		}
+	}
+}
+
+void Animation::Draw(Position2Int pos, bool isTurn) const
+{
+	if (m_handle == -1)
+	{
+		return; // アニメーションが設定されていないなら描画しない
+	}
+	
+	DrawRectRotaGraph(
+		pos.x, pos.y, // 表示座標(中央)
+		m_animCount * m_animSize.width, m_animIndex * m_animSize.height, // 画像の切り取り座標(左上)
+		m_animSize.width, m_animSize.height, // 切り取るサイズ
+		m_animScale, // 拡大率
+		m_animAngle, // 回転角度
+		m_handle, // 画像ハンドル
+		isTurn); // 反転するかどうか
+}
+
--- Projects/Utility/Animation.h
+++ Projects/Utility/Animation.h
@@ -1,17 +1,56 @@
 ﻿#pragma once
 
 /// <summary>
-/// アニメーション用シーン(仮)
+/// アニメーション用クラス
 /// </summary>
 class Animation
 {
 public:
+	/// <summary>
+	/// コンストラクタ
+	/// 何もアニメーションが入っていない状態で初期化される
+	/// </summary>
 	Animation();
 	virtual ~Animation();
 
+	
+	/// <summary>
+	/// アニメーションを設定する
+	/// </summary>
+	/// <param name="handle">アニメーションさせたい画像</param>
+	/// <param name="animSize">表示させたいサイズ</param>
+	/// <param name="animNum">表示するアニメーションが何枚あるか(1から数える)</param>
+	/// <param name="animFrame">アニメーションが動くフレーム数</param>
+	/// <param name="isLoop">ループするかどうか</param>
+	void SetAnimation(int handle, const Size& animSize, int animNum, int animFrame, bool isLoop);
 
+	/// <summary>
+	/// アニメーションの更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// アニメーションの描画処理
+	/// </summary>
+	/// <param name="pos">表示座標</param>
+	/// <param name="isTurn">画像の向きを反転するか</param>
+	void Draw(Position2Int pos, bool isTurn) const;
 
 private:
+	// アニメーションに必須の変数群
+	int m_handle; // アニメーションさせたい画像のハンドル
+	Size m_animSize; // アニメーションさせたい画像のサイズ
+	int m_animNum; // アニメーションさせたい画像の分割数
+	int m_animFrame; // アニメーションさせたい画像のフレーム数
+	int m_animIndex; // アニメーションさせたい画像の番号(Y座標)
+	bool m_isLoop; // アニメーションをループさせるかどうか
 
+	// アニメーションに必須でない変数群
+	float m_animScale; // アニメーションの拡大率
+	float m_animAngle; // アニメーションの回転角度
+
+	// アニメーション管理用変数群(このクラス内で完結する)
+	int m_frameCount; // フレームカウント
+	int m_animCount; // アニメーションカウント(現在アニメーションの何枚目か)
 };
 
