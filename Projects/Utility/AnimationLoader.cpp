5d23d40baa949542fec309370c5ff09de64e0d79
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-10 12:51:26 +0900

    アニメーションをロードするクラスを追加

---
+++ Projects/Utility/AnimationLoader.cpp
@@ -0,0 +1,22 @@
+﻿#include "Animation.h"
+#include <unordered_map>
+#include <string>
+#include "AnimationLoader.h"
+#include <fstream>
+#include <sstream>
+
+bool AnimationLoader::LoadAnimationData(const std::wstring& path, int graphHandle, std::unordered_map<std::string, Animation>& outAnimations)
+{
+	// ファイルを開く
+	std::ifstream ifs(path);
+
+	// ファイルが開けなかった場合はfalseを返す
+	if (!ifs.is_open()) return false;
+
+	std::string line;
+	while (std::getline(ifs, line))
+	{
+
+	}
+    return false;
+}
---
+++ Projects/Utility/AnimationLoader.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+
+/// <summary>
+/// ファイルからアニメーションデータをロードするクラス
+/// </summary>
+class AnimationLoader
+{
+public:
+	/// <summary>
+	/// アニメーションをファイルから読み込んで格納する
+	/// </summary>
+	/// <param name="path">指定のアニメーションデータ</param>
+	/// <param name="graphHandle">アニメーションの画像ハンドル</param>
+	/// <param name="outAnimations">返すアニメーション</param>
+	/// <returns></returns>
+	static bool LoadAnimationData(const std::wstring& path, int graphHandle, std::unordered_map<std::string, Animation>& outAnimations);
+
+};
+
