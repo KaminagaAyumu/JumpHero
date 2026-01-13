13318c08d5d96f7629734446afe668ef60128597
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-13 15:40:19 +0900

    イベントマネージャーの中身を実装中

--- Projects/Game/EventManager.cpp
+++ Projects/Game/EventManager.cpp
@@ -1,6 +1,7 @@
 ﻿#include "EventManager.h"
 
-EventManager::EventManager()
+EventManager::EventManager() : 
+	m_eventIndex(0)
 {
 }
 
@@ -14,6 +15,54 @@ void EventManager::Update()
 }
 
 void EventManager::Draw() const
+{
+
+}
+
+TriggerType EventManager::ToTriggerType(const std::string strData)
+{
+	if (strData == "GameStart") return TriggerType::GameStart;
+	if (strData == "EnterArea") return TriggerType::EnterArea;
+	if (strData == "TimeElapsed") return TriggerType::TimeElapsed;
+	if (strData == "EventEnd") return TriggerType::EventEnd;
+	if (strData == "OpenChest") return TriggerType::OpenChest;
+	if (strData == "GetItem") return TriggerType::GetItem;
+	if (strData == "EnemySpawn") return TriggerType::EnemySpawn;
+	if (strData == "PlayerPowerUp") return TriggerType::PlayerPowerUp;
+	if (strData == "NoTrigger") return TriggerType::NoTrigger;
+	return TriggerType::NoTrigger; // ここまで来たら不正な値なのでNoTriggerを返す
+}
+
+ActionType EventManager::ToActionType(const std::string strData)
+{
+	if (strData == "ShowText") return ActionType::ShowText;
+	if (strData == "DropItem") return ActionType::DropItem;
+	if (strData == "SpawnEnemy") return ActionType::SpawnEnemy;
+	if (strData == "FreezeGame") return ActionType::FreezeGame;
+	if (strData == "UnFreezeGame") return ActionType::UnFreezeGame;
+	if (strData == "FreezePlayer") return ActionType::FreezePlayer;
+	if (strData == "UnFreezePlayer") return ActionType::UnFreezePlayer;
+	if (strData == "LookCamera") return ActionType::LookCamera;
+	if (strData == "ReturnCamera") return ActionType::ReturnCamera;
+	if (strData == "PowerUp") return ActionType::PowerUp;
+	if (strData == "WaitInput") return ActionType::WaitInput;
+	if (strData == "SetBarrier") return ActionType::SetBarrier;
+	if (strData == "UnlockBarrier") return ActionType::UnlockBarrier;
+	if (strData == "ActiveGoal") return ActionType::ActiveGoal;
+	return ActionType::NoAction; // ここまで来たら不正な値なのでNoActionを返す
+}
+
+bool EventManager::CheckTrigger(const EventData& data)
+{
+	return false;
+}
+
+void EventManager::RunAction(const EventData& data)
+{
+
+}
+
+void EventManager::RunCommonAction(const EventData& data)
 {
 
 }
--- Projects/Game/EventManager.h
+++ Projects/Game/EventManager.h
@@ -17,5 +17,40 @@ public:
 
 private:
 
+	int m_eventIndex; // イベントの進行状況
+
+	/// <summary>
+	/// イベント条件を文字列から変換する用の関数
+	/// </summary>
+	/// <param name="strData">文字列データ</param>
+	/// <returns>変換後のイベント条件</returns>
+	TriggerType ToTriggerType(const std::string strData);
+
+	/// <summary>
+	/// イベントで行う内容を文字列から変換する用の関数
+	/// </summary>
+	/// <param name="strData">文字列データ</param>
+	/// <returns>変換後のイベント内容</returns>
+	ActionType ToActionType(const std::string strData);
+
+	/// <summary>
+	/// イベントトリガーを判定
+	/// </summary>
+	/// <param name="data">イベントデータ</param>
+	/// <returns>true : トリガー発火 false : トリガーが発火していない</returns>
+	bool CheckTrigger(const EventData& data);
+
+	/// <summary>
+	/// 指定されたイベントを行う
+	/// </summary>
+	/// <param name="data">イベントデータ</param>
+	void RunAction(const EventData& data);
+
+	/// <summary>
+	/// 指定された共通イベントを行う
+	/// </summary>
+	/// <param name="data">イベントデータ</param>
+	void RunCommonAction(const EventData& data);
+
 };
 
