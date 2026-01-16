89296d89311bc5906e34247b8ed47f368b268fa8
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-16 15:50:01 +0900

    セレクトリストUIの処理を実装

--- Projects/Utility/UI/UISelectList.cpp
+++ Projects/Utility/UI/UISelectList.cpp
@@ -1,30 +1,118 @@
 ﻿#include "UISelectList.h"
+#include "../StringFunction.h"
+#include "DxLib.h"
+
+namespace
+{
+	constexpr int kPaddingY = 10; // リストの上下端からテキストまでの余白
+}
 
 UISelectList::UISelectList() : 
 	m_pos{},
+	m_size{},
 	m_fontHandle(-1),
 	m_color(0),
-	m_frameCount(0)
+	m_itemSpacing(0),
+	m_frameCount(0),
+	m_cursor(0)
 {
+	m_items.clear();
 }
 
 UISelectList::~UISelectList()
 {
 }
 
-void UISelectList::Init(int handle, std::string text, const Position2& pos)
+void UISelectList::AddOption(const std::string& text, std::function<void()> onSelect)
 {
+	m_items.push_back(OptionItem{ text, std::move(onSelect) });
+
+	// 初回追加時にカーソルを0にする
+	if (m_items.size() == 1)
+	{
+		m_cursor = 0;
+	}
+}
+
+void UISelectList::Init(int fontHandle, Size size, const Position2& pos)
+{
+	m_fontHandle = fontHandle;
+	m_size = size;
+	m_pos = pos;
 }
 
 void UISelectList::Update()
 {
+	m_frameCount++;
 }
 
 void UISelectList::Draw() const
 {
+	// テキストとカーソルをサイズの範囲内に描画する
+	// テキストは左詰め
+	// 上から順に均等に項目を並べる
+	// 選択している項目が分かるようにするが、やり方(大きくしたり少し右寄りにするなど)は未定
+
+	const int halfW = m_size.width / 2;
+	const int halfH = m_size.height / 2;
+	const int top = m_pos.y - halfH;
+	const int bottom = m_pos.y + halfH;
+	const int left = m_pos.x - halfW;
+	const int right = m_pos.x + halfW;
+
+	int y = top + kPaddingY;
+
+	for (int i = 0; i < static_cast<int>(m_items.size()); i++)
+	{
+		const auto& text = m_items[i].text;
+		auto wText = StringFunction::WStringFromString(text); // ワイド文字列に変換
+		DrawStringToHandle(left + m_itemSpacing, y, wText.c_str(), m_color, m_fontHandle);
+		y += m_itemSpacing;
+		// y座標がサイズを超えたらループを抜ける
+		if (y > bottom - kPaddingY)
+		{
+			break;
+		}
+	}
+
 }
 
 bool UISelectList::IsAlive() const
 {
 	return false;
+}
+
+void UISelectList::MoveCursor(int dir)
+{
+	// 選択肢がない場合
+	if (m_items.empty() || dir == 0)
+	{
+		// カーソルを動かさない
+		return;
+	}
+	
+	// カーソルの処理の説明の仕方が分かりません
+	m_cursor = (m_cursor + dir + static_cast<int>(m_items.size())) % static_cast<int>(m_items.size());
+
+
+}
+
+void UISelectList::TriggerSelect()
+{
+	// 選択肢がない場合
+	if (m_items.empty())
+	{
+		return;
+	}
+
+	// 選択肢が選択できるものである場合
+	if (0 <= m_cursor && m_cursor < static_cast<int>(m_items.size()))
+	{
+		// 選択された際の処理が存在する場合
+		if (m_items[m_cursor].onSelect)
+		{
+			// 選択された処理を行う
+			m_items[m_cursor].onSelect();
+		}
+	}
 }
--- Projects/Utility/UI/UISelectList.h
+++ Projects/Utility/UI/UISelectList.h
@@ -8,7 +8,10 @@
 class UISelectList : public UIBase
 {
 public:
-	struct SelectItem
+	/// <summary>
+	/// 選択肢の内容
+	/// </summary>
+	struct OptionItem
 	{
 		std::string text; // 表示するテキストの内容
 		std::function<void()> onSelect; // 決定されたときに行う処理
@@ -16,22 +19,61 @@ public:
 
 	UISelectList();
 	virtual ~UISelectList();
+
 	/// <summary>
-	/// 初期化処理
+	/// リスト全体の初期化処理
 	/// </summary>
-	/// <param name="handle">テキストのハンドル</param>
-	/// <param name="text">表示するテキストの内容</param>
-	/// <param name="pos">表示する座標</param>
-	void Init(int handle, std::string text, const Position2& pos);
+	/// <param name="fontHandle">フォントのハンドル</param>
+	/// <param name="size">リスト全体の大きさ</param>
+	/// <param name="pos">リスト全体からの中心座標</param>
+	void Init(int fontHandle, Size size, const Position2& pos);
+
+	// リスト全体に直接関係のない変数を変更できる処理を追加したい(オプションごとに変わるようにしたい)
+	
+	/// <summary>
+	/// 選択肢を追加する
+	/// </summary>
+	/// <param name="text">選択肢の名前</param>
+	/// <param name="onSelect">選ばれた際の処理</param>
+	void AddOption(const std::string& text, std::function<void()> onSelect);
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
 	void Update()override;
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
 	void Draw()const override;
+
+	/// <summary>
+	/// 存在しているかどうかを返す
+	/// </summary>
+	/// <returns>true : 存在している false : 存在しない</returns>
 	bool IsAlive()const override;
+
+	/// <summary>
+	/// 上下方向にカーソルを動かす(値の数分動かす)
+	/// </summary>
+	/// <param name="dir">どの方向に動かすか(正の数で下に移動,負の数で上に移動)</param>
+	void MoveCursor(int dir);
+
+	/// <summary>
+	/// 選択された時の処理
+	/// </summary>
+	void TriggerSelect();
 
 private:
 	Position2 m_pos; // 表示座標(リスト全体の中心)
-	std::string m_text; // 表示するテキスト
+	Size m_size; // リスト全体のサイズ
+	std::vector<OptionItem> m_items; // 選択肢の内容をまとめる
 	int m_fontHandle; // フォントのハンドル
 	int m_color; // 文字の色
+	int m_itemSpacing; // 縦余白
+
 	int m_frameCount; // 表示してからのフレーム数
+
+	int m_cursor; // カーソルがどこにいるか
 };
 
