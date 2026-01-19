270f3a54adacdbcd2e500fdc5a7e91514b25e9da
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-19 13:22:54 +0900

    テキストのフォーマットが変わるテキストクラスを作成

---
+++ Projects/Utility/UI/UIFormatText.cpp
@@ -0,0 +1,79 @@
+﻿#include "UIFormatText.h"
+#include "../StringFunction.h"
+#include "DxLib.h"
+
+namespace
+{
+	constexpr int kDefaultColor = 0xffffff; // デフォルトの文字色(白)
+}
+
+UIFormatText::UIFormatText() :
+	m_pos{},
+	m_fontHandle(-1),
+	m_color(kDefaultColor),
+	m_frameCount(0),
+	m_isAlive(true)
+{
+}
+
+UIFormatText::~UIFormatText()
+{
+}
+
+void UIFormatText::Init(int handle, std::string text, const Position2& pos)
+{
+	m_fontHandle = handle;
+	m_text = text;
+	m_pos = pos;
+}
+
+void UIFormatText::Update()
+{
+	m_frameCount++;
+
+	// プロバイダがない場合処理を行わない
+	if (!m_provider) return;
+	// プロバイダをセット
+	CheckProvider();
+}
+
+void UIFormatText::Draw() const
+{
+	// テキストの内容をワイド文字列に変換
+	auto text = StringFunction::WStringFromString(m_text);
+	// テキストの幅を取得
+	int width = GetDrawStringWidthToHandle(text.c_str(), static_cast<int>(m_text.length()), m_fontHandle);
+	// テキストを中央揃えにするためにX座標を調整
+	int adjustedX = static_cast<int>(m_pos.x) - width / 2;
+	// テキストを描画
+	DrawStringToHandle(adjustedX, static_cast<int>(m_pos.y), text.c_str(), m_color, m_fontHandle);
+}
+
+bool UIFormatText::IsAlive() const
+{
+	return m_isAlive;
+}
+
+void UIFormatText::SetProvider(std::function<std::string()> provider)
+{
+	// プロバイダをセット
+	m_provider = std::move(provider);
+	// セットと同時に実際のテキストの内容も変更
+	CheckProvider();
+}
+
+void UIFormatText::CheckProvider()
+{
+	// プロバイダがセットされていない場合処理を抜ける
+	if (!m_provider) return;
+	
+	// 現在のテキストの内容をプロバイダからチェック
+	const std::string now = m_provider();
+	
+	// 現在のテキストのデータがプロバイダのテキストと一致していない場合
+	if (now != m_text)
+	{
+		// テキストを変更
+		m_text = now;
+	}
+}
---
+++ Projects/Utility/UI/UIFormatText.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "UIBase.h"
+#include "../Geometry.h"
+#include <string>
+#include <functional>
+
+
+class UIFormatText : public UIBase
+{
+public:
+	UIFormatText();
+	virtual ~UIFormatText();
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	/// <param name="handle">テキストのハンドル</param>
+	/// <param name="text">表示するテキストの内容</param>
+	/// <param name="pos">表示する座標</param>
+	void Init(int handle, std::string text, const Position2& pos);
+	void Update()override;
+	void Draw()const override;
+	bool IsAlive()const override;
+
+	/// <summary>
+	/// 表示するテキストを返すプロバイダをセットする
+	/// </summary>
+	/// <param name="provider">プロバイダ</param>
+	void SetProvider(std::function<std::string()> provider);
+
+private:
+	Position2 m_pos; // 表示座標
+	std::string m_text; // 表示するテキスト
+	std::function<std::string()> m_provider; // テキストの内容を取得するプロバイダ
+	int m_fontHandle; // フォントのハンドル
+	int m_color; // 文字の色
+	int m_frameCount; // 表示してからのフレーム数
+	bool m_isAlive; // 存在フラグ
+
+	/// <summary>
+	/// プロバイダからテキストデータを変更する
+	/// </summary>
+	void CheckProvider();
+
+};
+
