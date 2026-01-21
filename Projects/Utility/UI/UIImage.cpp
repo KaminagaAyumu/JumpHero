eb300987b8fb4e497af2a2a5aacf3fb2eae36a90
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-21 17:37:19 +0900

    イベントデータを変更

---
+++ Projects/Utility/UI/UIImage.cpp
@@ -0,0 +1,36 @@
+﻿#include "UIImage.h"
+#include "DxLib.h"
+
+UIImage::UIImage() : 
+	m_handle(-1),
+	m_size{},
+	m_pos{}
+{
+
+}
+
+UIImage::~UIImage()
+{
+}
+
+void UIImage::Init(int handle, const Size& size, const Position2& pos)
+{
+	m_handle = handle;
+	m_size = size;
+	m_pos;
+}
+
+void UIImage::Update()
+{
+
+}
+
+void UIImage::Draw() const
+{
+	
+}
+
+bool UIImage::IsAlive()const
+{
+	return true;
+}
---
+++ Projects/Utility/UI/UIImage.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "UIBase.h"
+#include "../Geometry.h"
+
+/// <summary>
+/// 画像を表示するクラス
+/// </summary>
+class UIImage : public UIBase
+{
+public:
+	UIImage();
+	virtual ~UIImage();
+
+	void Init(int handle, const Size& size, const Position2& pos);
+
+	void Update()override;
+
+	void Draw()const override;
+
+	bool IsAlive()const override;
+
+private:
+	int m_handle; // 画像ハンドル
+	Size m_size; // 画像のサイズ
+	Position2 m_pos; // 画像を表示する座標
+
+};
+
