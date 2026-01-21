3de55a1d98d8b99d33f3bd37e2eb66d4740911c3
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-01-21 22:39:02 +0900

    Effekseerのデータを管理するクラスを作成

---
+++ Projects/Game/Effect/EffekseerResourceManager.cpp
@@ -0,0 +1,42 @@
+﻿#include <map>
+#include <string>
+#include "EffekseerResourceManager.h"
+#include "EffekseerForDXLib.h"
+
+namespace
+{
+	constexpr int kResourceExist = 1; // リソースが存在していることを示す
+}
+
+int EffekseerResourceManager::LoadEffect(const std::wstring& path)
+{
+	// エフェクトのパスがすでにロード済みの場合
+	if (m_effekseerResources.contains(path))
+	{
+		auto ref = m_effekseerResources[path];
+		ref.refCounter++; // 参照カウンタを増やす
+		return ref.handle;
+	}
+	else
+	{
+		int handle = LoadEffekseerEffect(path.c_str()); // ハンドルを作成
+		m_effekseerResources[path].handle = handle; // ハンドルを設定
+		m_effekseerResources[path].refCounter = kResourceExist; // 参照カウンタを追加
+		return handle;
+	}
+}
+
+int EffekseerResourceManager::DeleteEffect(int handle)
+{
+	for (auto& res : m_effekseerResources)
+	{
+		if (handle == res.second.handle)
+		{
+			if (--res.second.refCounter <= 0) // 参照カウンタを減らして、最後の参照だったら
+			{
+				return DeleteEffekseerEffect(res.second.handle); // エフェクトハンドルを消去する
+			}
+			return kResourceExist; // 参照が存在しているので消去しない
+		}
+	}
+}
---
+++ Projects/Game/Effect/EffekseerResourceManager.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+/// <summary>
+/// Effekseerのデータを管理するクラス
+/// </summary>
+class EffekseerResourceManager
+{
+public:
+	/// <summary>
+	/// エフェクトをロードする
+	/// </summary>
+	/// <param name="path">エフェクトのパス</param>
+	/// <returns>エフェクトのハンドル</returns>
+	int LoadEffect(const std::wstring& path);
+
+	/// <summary>
+	/// エフェクトを消去する
+	/// </summary>
+	/// <param name="handle">エフェクトのハンドル</param>
+	/// <returns>0:成功 1:まだ参照先があるのでリソースの削除はしない -1:削除に失敗</returns>
+	int DeleteEffect(int handle);
+
+private:
+	struct EffekseerResourceInfo
+	{
+		int refCounter; // 参照カウンタ
+		int handle; // エフェクトのハンドル
+	};
+
+	// エフェクトのリソースを管理するマップ(文字列はエフェクトのパス)
+	std::map<std::wstring, EffekseerResourceInfo> m_effekseerResources;
+
+};
+
